91adc3af3662168577b2f2a9272ab8203946596b
John Li <johnli@johns-mbp.dhcp.wustl.edu>
2016-11-05 10:04:15 -0500

    Added a lot of partials

--- js/appRoutes.js
+++ js/appRoutes.js
@@ -6,6 +6,19 @@ appRoutes.config([
   '$stateProvider',
   '$locationProvider',
   function($stateProvider, $locationProvider) {
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: '../partials/home.html'
+      })
+      .state('patient', {
+        url:'/patient',
+        templateUrl: '../partials/patient.html'
+      })
+      .state('guide', {
+        url:'/guide',
+        templateUrl:'../partials/guide.html'
+      });
 
     $locationProvider.html5Mode(true);
   }]);
